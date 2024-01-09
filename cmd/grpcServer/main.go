7d985e02d20a330832abb226a5c59289cf8f4b02
Victor Kamei Kay <kameikay@gmail.com>
2024-01-09 06:11:32 -0300

    create grpc server

---
+++ cmd/grpcServer/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"database/sql"
+	"net"
+
+	"github.com/kameikay/grpc-example/internal/database"
+	"github.com/kameikay/grpc-example/internal/pb"
+	"github.com/kameikay/grpc-example/internal/service"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db, err := sql.Open("sqlit3", "./db.sqlite")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	categoryDB := database.NewCategory(db)
+	categoryService := service.NewCategoryService(*categoryDB)
+
+	grpcServer := grpc.NewServer()
+	reflection.Register(grpcServer)
+
+	pb.RegisterCategoryServiceServer(grpcServer, categoryService)
+
+	listener, err := net.Listen("tcp", ":50051")
+	if err := grpcServer.Serve(listener); err != nil {
+		panic(err)
+	}
+}
