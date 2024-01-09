27897d93a7aa89fa6d80b4c7c9a5f59d8948d3c4
Victor Kamei Kay <kameikay@gmail.com>
2024-01-09 07:03:03 -0300

    add create category stream

--- internal/service/category.go
+++ internal/service/category.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"io"
 
 	"github.com/kameikay/grpc-example/internal/database"
 	"github.com/kameikay/grpc-example/internal/pb"
@@ -68,4 +69,29 @@ func (c *CategoryService) GetCategory(ctx context.Context, input *pb.GetCategory
 	}
 
 	return categoryResponse, nil
+}
+
+func (c *CategoryService) CreateCategoryStream(stream pb.CategoryService_CreateCategoryStreamServer) error {
+	categories := &pb.CategoryList{}
+
+	for {
+		category, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(categories)
+		}
+		if err != nil {
+			return err
+		}
+
+		categoryResult, err := c.CategoryDB.Create(category.Name, category.Description)
+		if err != nil {
+			return err
+		}
+
+		categories.Categories = append(categories.Categories, &pb.Category{
+			Id:          categoryResult.ID,
+			Name:        categoryResult.Name,
+			Description: categoryResult.Description,
+		})
+	}
 }
